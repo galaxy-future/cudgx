2a346e95b9f88cc193309bd5fc9845bc6ad18f3c
yulong xin <xinyulong@galaxy-future.com>
2022-01-21 17:58:22 +0800

    更改pi逻辑

--- sample/pi/main.go
+++ sample/pi/main.go
@@ -141,20 +141,13 @@ func HandlerCalcPiHigh(c *gin.Context) {
 // pi launches n goroutines to compute an
 // approximation of pi.
 func pi(n int) float64 {
-	ch := make(chan float64)
-	for k := 0; k < n; k++ {
-		go term(ch, float64(k))
-	}
 	f := 0.0
 	for k := 0; k < n; k++ {
-		f += <-ch
+		f += 4 * math.Pow(-1, float64(k)) / (2*float64(k) + 1)
 	}
 	return f
 }
 
-func term(ch chan float64, k float64) {
-	ch <- 4 * math.Pow(-1, k) / (2*k + 1)
-}
 
 func initMetrics() {
 	latencyMin = metricGo.NewMonitoringMetric("latencyMin", []string{}, aggregate.NewMinBuilder())
